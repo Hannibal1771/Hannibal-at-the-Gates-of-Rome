d580bb0aa1b7c3bdc0b4fe26fa512a5df95538af
Hannibal1771 <36897389+Hannibal1771@users.noreply.github.com>
2018-10-04 00:28:19 +0300

    Add files via upload

---
+++ Программный продукт/Инфа/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Инфа
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Название = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Модель = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IPaddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Юзер = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ОС = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Процессор = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Видеокарта = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Оперативка = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ЖД = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IPHost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PublicIP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Юзер,
+            this.ОС,
+            this.Процессор,
+            this.Видеокарта,
+            this.Оперативка,
+            this.ЖД,
+            this.IP,
+            this.IPHost,
+            this.PublicIP});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1162, 143);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(386, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Получить информацию о своем ПК";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Название,
+            this.Модель,
+            this.IPaddress});
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 143);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(1162, 210);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // Название
+            // 
+            this.Название.HeaderText = "Имя устройства";
+            this.Название.Name = "Название";
+            // 
+            // Модель
+            // 
+            this.Модель.HeaderText = "ID";
+            this.Модель.Name = "Модель";
+            // 
+            // IPaddress
+            // 
+            this.IPaddress.HeaderText = "Число подключений";
+            this.IPaddress.Name = "IPaddress";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(341, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Получить информацию о устройствах в сети";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Юзер
+            // 
+            this.Юзер.HeaderText = "Пользователь";
+            this.Юзер.Name = "Юзер";
+            // 
+            // ОС
+            // 
+            this.ОС.HeaderText = "Операционная система";
+            this.ОС.Name = "ОС";
+            this.ОС.Width = 200;
+            // 
+            // Процессор
+            // 
+            this.Процессор.HeaderText = "Процессор";
+            this.Процессор.Name = "Процессор";
+            this.Процессор.Width = 200;
+            // 
+            // Видеокарта
+            // 
+            this.Видеокарта.HeaderText = "Видеокарта";
+            this.Видеокарта.Name = "Видеокарта";
+            this.Видеокарта.Width = 200;
+            // 
+            // Оперативка
+            // 
+            this.Оперативка.HeaderText = "Размер оперативной памяти";
+            this.Оперативка.Name = "Оперативка";
+            // 
+            // ЖД
+            // 
+            this.ЖД.HeaderText = "Жёсткий диск";
+            this.ЖД.Name = "ЖД";
+            this.ЖД.Width = 200;
+            // 
+            // IP
+            // 
+            this.IP.HeaderText = "Локальный IP";
+            this.IP.Name = "IP";
+            // 
+            // IPHost
+            // 
+            this.IPHost.HeaderText = "Хост";
+            this.IPHost.Name = "IPHost";
+            // 
+            // PublicIP
+            // 
+            this.PublicIP.HeaderText = "Публичный IP";
+            this.PublicIP.Name = "PublicIP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1162, 353);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Название;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Модель;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IPaddress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Юзер;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ОС;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Процессор;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Видеокарта;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Оперативка;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ЖД;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IPHost;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PublicIP;
+    }
+}
+
---
+++ Программный продукт/Инфа/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Net;
+
+namespace Инфа
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows[0].SetValues(Environment.UserName);
+
+            ManagementObjectSearcher OS = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_OperatingSystem");
+            foreach (ManagementObject queryObj in OS.Get())
+            {
+                dataGridView1.Rows[0].Cells[1].Value = queryObj["Name"];
+            }
+
+            ManagementObjectSearcher Proc = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+            foreach (ManagementObject queryObj in Proc.Get())
+            {
+                dataGridView1.Rows[0].Cells[2].Value = queryObj["Name"];
+            }
+
+            ManagementObjectSearcher Videokarta = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_VideoController");
+            foreach (ManagementObject queryObj in Videokarta.Get())
+            {
+                dataGridView1.Rows[0].Cells[3].Value = queryObj["Name"];
+            }
+
+            ManagementObjectSearcher Oper= new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_PhysicalMemory");
+            foreach (ManagementObject queryObj in Oper.Get())
+            {
+                dataGridView1.Rows[0].Cells[4].Value = Math.Round(System.Convert.ToDouble(queryObj["Capacity"]) / 1024 / 1024 / 1024 / 2);
+            }
+
+            ManagementObjectSearcher Zdisk = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_DiskDrive");
+            foreach (ManagementObject queryObj in Zdisk.Get())
+            {
+                dataGridView1.Rows[0].Cells[5].Value = queryObj["Model"];
+            }
+
+            
+            string Host = System.Net.Dns.GetHostName();
+            string IP = System.Net.Dns.GetHostByName(Host).AddressList[0].ToString();
+            
+            try
+            {
+                 string pubIP = new System.Net.WebClient().DownloadString("https://api.ipify.org");
+                dataGridView1.Rows[0].Cells[8].Value = pubIP;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Подключение к Интернету отсутствует", "Ошибка");
+                dataGridView1.Rows[0].Cells[8].Value = "Кабель не подключен";
+            }
+            dataGridView1.Rows[0].Cells[7].Value = Host;
+            
+            dataGridView1.Rows[0].Cells[6].Value = IP;
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ManagementObjectSearcher searcher7 = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_NetworkLoginProfile");
+            foreach (ManagementObject queryObj in searcher7.Get())
+            {
+                dataGridView2.Rows[0].SetValues (queryObj["Name"]);
+            }
+
+            ManagementObjectSearcher searcher8 = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_NetworkLoginProfile");
+            foreach (ManagementObject queryObj in searcher8.Get())
+            {
+                dataGridView2.Rows[0].Cells[1].Value = queryObj["UserID"];
+            }
+            ManagementObjectSearcher searcher9 = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_NetworkLoginProfile");
+            foreach (ManagementObject queryObj in searcher9.Get())
+            {
+                dataGridView2.Rows[0].Cells[2].Value = queryObj["NumberOfLogons"];
+            }
+        }
+    }
+}
